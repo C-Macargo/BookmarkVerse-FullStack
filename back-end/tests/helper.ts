a06744ebb7f071558e418c6cebba38d26bf9a5a3
C-Macargo <caio.camargos97@gmail.com>
2023-07-17 14:22:22 -0300

    chore(back-end): clean db after tests

--- back-end/tests/helper.ts
+++ back-end/tests/helper.ts
@@ -1,10 +1,9 @@
 import prisma from '@/config/database';
 
-
 export async function cleanDb() {
-    await prisma.session.deleteMany({})
-    await prisma.bookmark.deleteMany({})
-    await prisma.review.deleteMany({})
-    await prisma.book.deleteMany({})
-    await prisma.user.deleteMany({})
+  await prisma.session.deleteMany({});
+  await prisma.bookmark.deleteMany({});
+  await prisma.review.deleteMany({});
+  await prisma.book.deleteMany({});
+  await prisma.user.deleteMany({});
 }
