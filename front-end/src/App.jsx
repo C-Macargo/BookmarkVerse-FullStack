ae06e92a36ccdf419a5832d8a68e1adc958ab728
C-Macargo <caio.camargos97@gmail.com>
2023-08-01 12:37:30 -0300

    feat(front-end): adding react-toastify library for alerts

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,13 @@
+import { CustomToastContainer } from "./components/CustomToastContainer";
 import { UserProvider } from "./components/contexts/userContext"
 import Header from "./components/headerComponents/Header"
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
     <>
+    <CustomToastContainer/>
     <UserProvider>
     <Header/>
     </UserProvider>
