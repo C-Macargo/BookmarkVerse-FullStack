7b1fb0e8d5444ae3000fefc417dece6107f3746f
C-Macargo <caio.camargos97@gmail.com>
2023-09-04 13:23:48 -0300

    fix(front-end): returning to home when clicking the logo

--- front-end/src/components/headerComponents/Logo.jsx
+++ front-end/src/components/headerComponents/Logo.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 function Logo() {
+
+	const navigate = useNavigate()
+
 	return (
 		<div className=" inline-flex">
 			<img
+				onClick={() => navigate("/")}
 				src="https://i.imgur.com/oPx2tjf.png"
 				className="w-16 h-12 -mt-2 -mb-2">
             </img>
-			<h1 className="text-white text-3xl font-bold hover:text-purple-600 transition duration-1000 ease-in-out">
+			<h1 
+				onClick={() => navigate("/")}	
+				className="text-white text-3xl font-bold hover:text-purple-600 transition duration-1000 ease-in-out">
 				BookmarkVerse
 			</h1>
 		</div>
